8397c96a86ffa3016313d5b0d7327bbf7f87cd31
nick <n.weldin@mdx.ac.uk>
2021-02-07 21:42:31 +0000

    added simple read of RXFIFOfrom PIO to test values coming through

--- encoders.c
+++ encoders.c
@@ -4,18 +4,23 @@
 #include "hardware/pio.h"
 #include "encoders.pio.h"
 
-#define HARD_UART_INST uart1
+
 
 
 
 #define PIO_ENCODER_PIN 3
 
 int main() {
-
+    stdio_init_all();
     // Set up the state machine.
     PIO pio = pio0;
     uint sm = 0;
     uint offset = pio_add_program(pio, &encoders_program);
     encoders_program_init(pio, sm, offset, PIO_ENCODER_PIN);
-
+    while (true){
+        if (pio_sm_get_rx_fifo_level(pio, sm)>0){
+            uint32_t val = pio_sm_get(pio, sm);
+            printf("%d", val);
+        }
+    }
 }
