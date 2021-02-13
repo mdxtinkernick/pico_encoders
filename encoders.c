51bbdbe5971b6920bbb78c69fd68e09ed57b45a8
nick <n.weldin@mdx.ac.uk>
2021-02-13 18:00:17 +0000

    created 4 and used for loop to read val

--- encoders.c
+++ encoders.c
@@ -5,33 +5,38 @@
 
 #define SM1_ENCODER_PIN 14
 #define SM2_ENCODER_PIN 12
+#define SM3_ENCODER_PIN 10
+#define SM4_ENCODER_PIN 8
 
 int main() {
     stdio_init_all();
     // Set up the state machine.
     PIO pio = pio0;
-    uint sm1 = 0;
-    uint sm2 = 1;
+    
+    uint32_t val[] = {0, 0, 0, 0};
 
-    uint32_t val1 = 0;
-    uint32_t val2 = 1;
+    uint sm1 =0;
+    uint sm2 =1;
+    uint sm3 =2;
+    uint sm4 =3;
 
     uint offset = pio_add_program(pio, &encoders_program);
     encoders_program_init(pio, sm1, offset, SM1_ENCODER_PIN);
     encoders_program_init(pio, sm2, offset, SM2_ENCODER_PIN);
+    encoders_program_init(pio, sm3, offset, SM3_ENCODER_PIN);
+    encoders_program_init(pio, sm4, offset, SM4_ENCODER_PIN);
 
     while (true){
         bool change = false; 
-        if (pio_sm_get_rx_fifo_level(pio, sm1)>0){
-            uint32_t val1 = pio_sm_get(pio, sm1);
+        for (uint i = 0; i<4; i++){
+        if (pio_sm_get_rx_fifo_level(pio, i)>0){
+            uint32_t val1 = pio_sm_get(pio, i);
             change = true;
         }
-        if (pio_sm_get_rx_fifo_level(pio, sm2)>0){
-            uint32_t val2 = pio_sm_get(pio, sm2);
-            change = true;
         }
+        
         if (change){
-            printf("%lu %lu\n", val1, val2);
+            printf("%lu %lu %lu %lu\n", val[0], val[1], val[2], val[3]);
             change = false;
         }
     }
