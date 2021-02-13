924f378c2f56e2c1ec2da5eb0e420e04bd7ce72f
nick <n.weldin@mdx.ac.uk>
2021-02-13 17:48:54 +0000

    added second state machine

--- encoders.c
+++ encoders.c
@@ -3,19 +3,36 @@
 #include "hardware/pio.h"
 #include "encoders.pio.h"
 
-#define PIO_ENCODER_PIN 14
+#define SM1_ENCODER_PIN 14
+#define SM2_ENCODER_PIN 12
 
 int main() {
     stdio_init_all();
     // Set up the state machine.
     PIO pio = pio0;
-    uint sm = 0;
+    uint sm1 = 0;
+    uint sm2 = 1;
+
+    uint32_t val1 = 0;
+    uint32_t val2 = 1;
+
     uint offset = pio_add_program(pio, &encoders_program);
-    encoders_program_init(pio, sm, offset, PIO_ENCODER_PIN);
+    encoders_program_init(pio, sm1, offset, SM1_ENCODER_PIN);
+    encoders_program_init(pio, sm2, offset, SM2_ENCODER_PIN);
+
     while (true){
-        if (pio_sm_get_rx_fifo_level(pio, sm)>0){
-            uint32_t val = pio_sm_get(pio, sm);
-            printf("%lu\n", val);
+        bool change = false; 
+        if (pio_sm_get_rx_fifo_level(pio, sm1)>0){
+            uint32_t val1 = pio_sm_get(pio, sm1);
+            change = true;
+        }
+        if (pio_sm_get_rx_fifo_level(pio, sm2)>0){
+            uint32_t val2 = pio_sm_get(pio, sm2);
+            change = true;
+        }
+        if (change){
+            printf("%lu %lu\n", val1, val2);
+            change = false;
         }
     }
 }
