5f3410ba7a0d1201b17f02e2191f055c740c5f31
nick <n.weldin@mdx.ac.uk>
2021-02-15 19:24:56 +0000

    working, but stops once the number of transfers reached, so for now set that to 0xFFFFFFFF

--- encoders.c
+++ encoders.c
@@ -11,12 +11,23 @@ int main() {
     // Set up the state machine.
     PIO pio = pio0;
     uint sm = 0;
+    uint dma_chan = 0;
+    uint32_t capture_buf = 0;
     uint offset = pio_add_program(pio, &encoders_program);
     encoders_program_init(pio, sm, offset, PIO_ENCODER_PIN);
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_read_increment(&c, false);
+    channel_config_set_write_increment(&c, false);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
+    dma_channel_configure(dma_chan, &c,
+        &capture_buf,        // Destinatinon pointer
+        &pio->rxf[sm],      // Source pointer
+        0xFFFFFFFF, // Number of transfers
+        true                // Start immediately
+    );
+
     while (true){
-        if (pio_sm_get_rx_fifo_level(pio, sm)>0){
-            uint32_t val = pio_sm_get(pio, sm);
-            printf("%lu\n", val);
-        }
+        printf("%lu\n", capture_buf);
+        sleep_ms(1000);
     }
 }
