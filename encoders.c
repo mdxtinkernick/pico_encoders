b264b7bb744fd17888f6e8d5d79f4da6a5b1a646
nick <n.weldin@mdx.ac.uk>
2021-02-16 17:02:01 +0000

    tidied up whitespace and added variable for number of encoders

--- encoders.c
+++ encoders.c
@@ -7,28 +7,25 @@
 
 
 uint encoder_pin[] = {14, 16, 18, 20, 2, 4, 6, 8};
+uint number_of_encoders =8;
 
 PIO pio;
 PIO pio2;
-uint regist = 0;
 uint reg2 = 0;
 
 void dma_handler() {
     uint i = 1;
     int interrupt_channel = 0; 
-        while ((i & dma_hw->ints0) == 0) {  
-            i = i << 1; 
-            ++interrupt_channel; 
-            }  
-    regist = dma_hw->ints0;
+    while ((i & dma_hw->ints0) == 0) {  
+        i = i << 1; 
+        ++interrupt_channel; 
+    } 
     dma_hw->ints0 = 1u << interrupt_channel;
-    reg2 = dma_hw->ints0;
     if (interrupt_channel < 4){
         dma_channel_set_read_addr(interrupt_channel, &pio->rxf[interrupt_channel], true);
     }else{
         dma_channel_set_read_addr(interrupt_channel, &pio2->rxf[interrupt_channel%4], true);
-    }
-    
+    } 
 }
 
 int main() {
@@ -42,43 +39,41 @@ int main() {
     uint offset = pio_add_program(pio, &encoders_program);
     uint offset2 = pio_add_program(pio2, &encoders_program);
     
-    for (int i = 0; i<8; i++){
+    for (int i = 0; i<number_of_encoders; i++){
         if (i<4){
-    encoders_program_init(pio, i, offset, encoder_pin[i]);
+            encoders_program_init(pio, i, offset, encoder_pin[i]);
         }else{
-    encoders_program_init(pio2, i%4, offset2, encoder_pin[i]);
-        }
-    
-    dma_channel_config c = dma_channel_get_default_config(i);
-    channel_config_set_read_increment(&c, false);
-    channel_config_set_write_increment(&c, false);
-    channel_config_set_dreq(&c, pio_get_dreq(pio, i, false));
-    if (i<4){
-    dma_channel_configure(i, &c,
-        &capture_buf[i],        // Destinatinon pointer
-        &pio->rxf[i],      // Source pointer
-        0x10, // Number of transfers
-        true                // Start immediately
-        );
+            encoders_program_init(pio2, i%4, offset2, encoder_pin[i]);
+        }  
+        dma_channel_config c = dma_channel_get_default_config(i);
+        channel_config_set_read_increment(&c, false);
+        channel_config_set_write_increment(&c, false);
+        channel_config_set_dreq(&c, pio_get_dreq(pio, i, false));
+        if (i<4){
+            dma_channel_configure(i, &c,
+                &capture_buf[i],        // Destinatinon pointer
+                &pio->rxf[i],      // Source pointer
+                0x10, // Number of transfers
+                true                // Start immediately
+            );
         }else{
-    dma_channel_configure(i, &c,
-        &capture_buf[i],        // Destinatinon pointer
-        &pio2->rxf[i%4],      // Source pointer
-        0x10, // Number of transfers
-        true                // Start immediately
-        );
+            dma_channel_configure(i, &c,
+                &capture_buf[i],        // Destinatinon pointer
+                &pio2->rxf[i%4],      // Source pointer
+                0x10, // Number of transfers
+                true                // Start immediately
+            );
         }
-    
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
-    irq_set_enabled(DMA_IRQ_0, true);
-    dma_channel_set_irq0_enabled(i, true);
+        irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
+        irq_set_enabled(DMA_IRQ_0, true);
+        dma_channel_set_irq0_enabled(i, true);
     }
 
-  
-
-    while (true){
-        
-        printf("%lu %lu %lu %lu\n", capture_buf[0], capture_buf[1], capture_buf[2], capture_buf[3]);
+    while (true){   
+        for (int i = 0; i < number_of_encoders; i++){
+            printf("%lu ", capture_buf[i]);
+        }
+        printf("\n");
         sleep_ms(10);
     }
 }
